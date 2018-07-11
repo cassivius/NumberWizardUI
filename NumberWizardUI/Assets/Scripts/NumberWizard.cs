5add74317e6c94ab4829008efc2aaa98a6ec3b8d
Casey Sledge <cybermanforn@gmail.com>
2018-07-11 11:09:04 -0500

    Imported/refactored NumberWizard; updated game scene with buttons and colors

---
+++ NumberWizardUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartGame();
+    }
+	
+    public void OnPressHigher () {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower () {
+        max = guess;
+        NextGuess();
+    }
+    void StartGame()
+    {
+        max += 1;
+        guess = (max + min) / 2;
+    }
+
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void NextGuess ()
+    {
+        guess = (min + max) / 2;
+        Debug.Log("Is your number higher or lower than : " + guess);
+    }
+}
---
+++ NumberWizardUI/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadNextScene()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(SceneManager.sceneCountInBuildSettings);
+        sceneIndex = sceneIndex == SceneManager.sceneCountInBuildSettings - 1 ? 0 : sceneIndex;
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+}
